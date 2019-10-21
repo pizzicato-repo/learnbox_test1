853409f625a250333babd86a04f9b1a54ca6435f
amine <elmregl@gmail.com>
2019-10-21 23:01:48 +0200

    il est tard mais ca semble ok

---
+++ inscription_pedago/test_views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from inscription_pedago.models import Student, Teatcher
+from django.utils import timezone
+
+
+def index(request):
+    return HttpResponse("Hello, world : LearnBox initiative.")
+
+def create_student(request):
+    s = Student( name="toto", inscription_date=timezone.now() )
+    s.save()
+    #all_students = Student.objects.all()
+    #txt = "il y a "+ len(all_students) + "students"
+    return HttpResponse( 'txt' )
+
+def test(request):
+    return HttpResponse( "test" )
+
