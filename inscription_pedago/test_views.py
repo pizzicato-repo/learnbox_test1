f0ac9689527b5bb4e75fade1b3456547f894b3ee
mine <elmregl.gmailgit>
2019-10-22 06:05:11 +0200

    correction str

--- inscription_pedago/test_views.py
+++ inscription_pedago/test_views.py
@@ -12,8 +12,8 @@ def create_student(request):
     s = Student( name="toto", inscription_date=timezone.now() )
     s.save()
     all_students = Student.objects.all()
-    txt = "il y a "+ len( str(all_students)) + " students dans la base actuellement."
-    return HttpResponse( 'txt' )
+    txt = "il y a "+ str( len(all_students)) + " students dans la base actuellement."
+    return HttpResponse( txt )
 
 def test(request):
     return HttpResponse( "test" )
