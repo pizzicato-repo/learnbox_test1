b28c60e6f5571e07898ea13b3ec0bede35b252c9
amine <elmregl@gmail.com>
2019-10-22 05:50:25 +0200

    correction str

--- inscription_pedago/test_views.py
+++ inscription_pedago/test_views.py
@@ -11,8 +11,8 @@ def index(request):
 def create_student(request):
     s = Student( name="toto", inscription_date=timezone.now() )
     s.save()
-    #all_students = Student.objects.all()
-    #txt = "il y a "+ len(all_students) + "students"
+    all_students = Student.objects.all()
+    txt = "il y a "+ len( str(all_students)) + " students dans la base actuellement."
     return HttpResponse( 'txt' )
 
 def test(request):
