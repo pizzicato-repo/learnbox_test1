04fb9a86368e4ffb99cfa77c5c25f1d34072ea38
mine <elmregl.gmailgit>
2019-10-30 14:35:28 +0100

    rajout administrations

--- inscription_pedago/admin.py
+++ inscription_pedago/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student, Teatcher, Discipline
+
+admin.site.register(Student)
+admin.site.register(Teatcher)
+admin.site.register(Discipline)
--- inscription_pedago/models.py
+++ inscription_pedago/models.py
@@ -11,6 +11,21 @@ class Student(models.Model):
     def get_absolute_url(self):
         return reverse('student-detail', kwargs={'pk': self.pk})
 
+    def __str__(self):
+        return self.surname +' '+ self.name +' ('+ self.email +')'
+
+class Discipline(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Teatcher(models.Model):
     name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    url_site = models.CharField(max_length=200, default='url_vide')
 
+    disciplines = models.ManyToManyField(Discipline)
+
+    def __str__(self):
+        return self.surname +' '+ self.name
