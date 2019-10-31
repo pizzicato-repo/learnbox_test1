d8d5c103a59397d565569c15ea755aa30640935f
mine <elmregl.gmailgit>
2019-10-31 04:26:35 +0100

    merge migrations'

--- inscription_pedago/models.py
+++ inscription_pedago/models.py
@@ -25,7 +25,7 @@ class Discipline(models.Model):
 
 class Teatcher(models.Model):
     name = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200, default='default_surname')
     url_site = models.URLField(max_length=200, default='url_vide')
 
     disciplines = models.ManyToManyField(Discipline)
